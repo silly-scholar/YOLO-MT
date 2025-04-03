8f7f69ccb8caf42b5a68423b6ce1c9a0c8318180
silly-scholar <fdc6317@163.com>
2025-04-03 15:37:07 +0800

    Add files via upload

---
+++ YOLO-MT.py
@@ -0,0 +1,420 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class C1(nn.Module):
+    """CSP Bottleneck with 1 convolution."""
+
+    def __init__(self, c1, c2, n=1):
+        """Initializes the CSP Bottleneck with configurations for 1 convolution with arguments ch_in, ch_out, number."""
+        super().__init__()
+        self.cv1 = Conv(c1, c2, 1, 1)
+        self.m = nn.Sequential(*(Conv(c2, c2, 3) for _ in range(n)))
+
+    def forward(self, x):
+        """Applies cross-convolutions to input in the C3 module."""
+        y = self.cv1(x)
+        return self.m(y) + y
+
+
+class C2(nn.Module):
+    """CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initializes a CSP Bottleneck with 2 convolutions and optional shortcut connection."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv(2 * self.c, c2, 1)  # optional act=FReLU(c2)
+        # self.attention = ChannelAttention(2 * self.c)  # or SpatialAttention()
+        self.m = nn.Sequential(*(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck with 2 convolutions."""
+        a, b = self.cv1(x).chunk(2, 1)
+        return self.cv2(torch.cat((self.m(a), b), 1))
+
+
+class C2f(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initializes a CSP bottleneck with 2 convolutions and n Bottleneck blocks for faster processing."""
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+class C3(nn.Module):
+    """CSP Bottleneck with 3 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initialize the CSP Bottleneck with given channels, number, shortcut, groups, and expansion values."""
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, 1, 1)
+        self.cv2 = Conv(c1, c_, 1, 1)
+        self.cv3 = Conv(2 * c_, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=((1, 1), (3, 3)), e=1.0) for _ in range(n)))
+
+    def forward(self, x):
+        """Forward pass through the CSP bottleneck with 2 convolutions."""
+        return self.cv3(torch.cat((self.m(self.cv1(x)), self.cv2(x)), 1))
+
+
+class C3x(C3):
+    """C3 module with cross-convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5):
+        """Initialize C3TR instance and set default parameters."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.c_ = int(c2 * e)
+        self.m = nn.Sequential(*(Bottleneck(self.c_, self.c_, shortcut, g, k=((1, 3), (3, 1)), e=1) for _ in range(n)))
+
+class C3k2(C2f):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        """Initializes the C3k2 module, a faster CSP Bottleneck with 2 convolutions and optional C3k blocks."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(
+            C3k(self.c, self.c, 2, shortcut, g) if c3k else Bottleneck(self.c, self.c, shortcut, g) for _ in range(n)
+        )
+
+
+class C3k(C3):
+    """C3k is a CSP bottleneck module with customizable kernel sizes for feature extraction in neural networks."""
+
+    def __init__(self, c1, c2, n=1, shortcut=True, g=1, e=0.5, k=3):
+        """Initializes the C3k module with specified channels, number of layers, and configurations."""
+        super().__init__(c1, c2, n, shortcut, g, e)
+        c_ = int(c2 * e)  # hidden channels
+        # self.m = nn.Sequential(*(RepBottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+        self.m = nn.Sequential(*(Bottleneck(c_, c_, shortcut, g, k=(k, k), e=1.0) for _ in range(n)))
+######################################## HierarchicalEdgeRefinement start ########################################
+class EdgeRefinement(nn.Module):
+    def __init__(self, in_dim):
+        super().__init__()
+        self.out_conv = Conv(in_dim, in_dim, act=nn.Sigmoid())
+        self.pool = nn.AvgPool2d(3, stride=1, padding=1)
+
+    def forward(self, x):
+        edge = self.pool(x)
+        edge = x - edge
+        edge = self.out_conv(edge)
+        return x + edge
+
+
+class HierarchicalEdgeRefinement(nn.Module):
+    def __init__(self, inc, bins):
+        super().__init__()
+
+        self.features = []
+        for bin in bins:
+            self.features.append(nn.Sequential(
+                nn.AdaptiveAvgPool2d(bin),
+                Conv(inc, inc // len(bins), 1),
+                Conv(inc // len(bins), inc // len(bins), 3, g=inc // len(bins))
+            ))
+        self.ees = []
+        for _ in bins:
+            self.ees.append(EdgeRefinement(inc // len(bins)))
+        self.features = nn.ModuleList(self.features)
+        self.ees = nn.ModuleList(self.ees)
+        self.local_conv = Conv(inc, inc, 3)
+        self.final_conv = Conv(inc * 2, inc)
+
+    def forward(self, x):
+        x_size = x.size()
+        out = [self.local_conv(x)]
+        for idx, f in enumerate(self.features):
+            out.append(self.ees[idx](F.interpolate(f(x), x_size[2:], mode='bilinear', align_corners=True)))
+        return self.final_conv(torch.cat(out, 1))
+
+
+
+
+class C3k_HierarchicalEdgeRefinement(C3k):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=3):
+        super().__init__(c1, c2, n, shortcut, g, e, k)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(HierarchicalEdgeRefinement(c_, [3, 6, 9, 12]) for _ in range(n)))
+
+
+class C3k2_HierarchicalEdgeRefinement(C3k2):
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        super().__init__(c1, c2, n, c3k, e, g, shortcut)
+        self.m = nn.ModuleList(C3k_HierarchicalEdgeRefinement(self.c, self.c, 2, shortcut,
+                                                                    g) if c3k else HierarchicalEdgeRefinement(
+            self.c, [3, 6, 9, 12]) for _ in range(n))
+
+
+######################################## MutilScaleEdgeInformationEnhance end ########################################
+
+######################################## FFCM start ########################################
+
+class FourierUnit(nn.Module):
+
+    def __init__(self, in_channels, out_channels, groups=1):
+        super(FourierUnit, self).__init__()
+        self.groups = groups
+        # self.conv_layer = torch.nn.Conv2d(in_channels=in_channels * 2, out_channels=out_channels * 2,
+        #                                   kernel_size=1, stride=1, padding=0, groups=self.groups, bias=False)
+        # self.bn = torch.nn.BatchNorm2d(out_channels * 2)
+        # self.relu = torch.nn.ReLU(inplace=True)
+
+        self.conv = Conv(in_channels * 2, out_channels * 2, 1, g=groups, act=nn.ReLU(inplace=True))
+
+    def forward(self, x):
+        batch, c, h, w = x.size()
+
+        # (batch, c, h, w/2+1, 2)
+        ffted = torch.fft.rfft2(x, norm='ortho')
+        x_fft_real = torch.unsqueeze(torch.real(ffted), dim=-1)
+        x_fft_imag = torch.unsqueeze(torch.imag(ffted), dim=-1)
+        ffted = torch.cat((x_fft_real, x_fft_imag), dim=-1)
+        # (batch, c, 2, h, w/2+1)
+        ffted = ffted.permute(0, 1, 4, 2, 3).contiguous()
+        ffted = ffted.view((batch, -1,) + ffted.size()[3:])
+
+        # ffted = self.conv_layer(ffted)  # (batch, c*2, h, w/2+1)
+        # ffted = self.relu(self.bn(ffted))
+        ffted = self.conv(ffted)
+
+        ffted = ffted.view((batch, -1, 2,) + ffted.size()[2:]).permute(
+            0, 1, 3, 4, 2).contiguous()  # (batch,c, t, h, w/2+1, 2)
+        ffted = torch.view_as_complex(ffted)
+
+        output = torch.fft.irfft2(ffted, s=(h, w), norm='ortho')
+
+        return output
+
+
+class Freq_Fusion(nn.Module):
+    def __init__(
+            self,
+            dim,
+            kernel_size=[1, 3, 5, 7],
+            se_ratio=4,
+            local_size=8,
+            scale_ratio=2,
+            spilt_num=4
+    ):
+        super(Freq_Fusion, self).__init__()
+        self.dim = dim
+        self.c_down_ratio = se_ratio
+        self.size = local_size
+        self.dim_sp = dim * scale_ratio // spilt_num
+        self.conv_init_1 = nn.Sequential(  # PW
+            nn.Conv2d(dim, dim, 1),
+            nn.GELU()
+        )
+        self.conv_init_2 = nn.Sequential(  # DW
+            nn.Conv2d(dim, dim, 1),
+            nn.GELU()
+        )
+        self.conv_mid = nn.Sequential(
+            nn.Conv2d(dim * 2, dim, 1),
+            nn.GELU()
+        )
+        self.FFC = FourierUnit(self.dim * 2, self.dim * 2)
+
+        self.bn = torch.nn.BatchNorm2d(dim * 2)
+        self.relu = torch.nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        x_1, x_2 = torch.split(x, self.dim, dim=1)
+        x_1 = self.conv_init_1(x_1)
+        x_2 = self.conv_init_2(x_2)
+        x0 = torch.cat([x_1, x_2], dim=1)
+        x = self.FFC(x0) + x0
+        x = self.relu(self.bn(x))
+
+        return x
+
+
+class Fused_Fourier_Conv_Mixer(nn.Module):
+    def __init__(
+            self,
+            dim,
+            token_mixer_for_gloal=Freq_Fusion,
+            mixer_kernel_size=[1, 3, 5, 7],
+            local_size=8
+    ):
+        super(Fused_Fourier_Conv_Mixer, self).__init__()
+        self.dim = dim
+        self.mixer_gloal = token_mixer_for_gloal(dim=self.dim, kernel_size=mixer_kernel_size,
+                                                 se_ratio=8, local_size=local_size)
+
+        self.ca_conv = nn.Sequential(
+            nn.Conv2d(2 * dim, dim, 1),
+            nn.Conv2d(dim, dim, kernel_size=3, padding=1, groups=dim, padding_mode='reflect'),
+            nn.GELU()
+        )
+        self.ca = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(dim, dim // 4, kernel_size=1),
+            nn.GELU(),
+            nn.Conv2d(dim // 4, dim, kernel_size=1),
+            nn.Sigmoid()
+        )
+        self.conv_init = nn.Sequential(  # PW->DW->
+            nn.Conv2d(dim, dim * 2, 1),
+            nn.GELU()
+        )
+        self.dw_conv_1 = nn.Sequential(
+            nn.Conv2d(self.dim, self.dim, kernel_size=3, padding=3 // 2,
+                      groups=self.dim, padding_mode='reflect'),
+            nn.GELU()
+        )
+        self.dw_conv_2 = nn.Sequential(
+            nn.Conv2d(self.dim, self.dim, kernel_size=5, padding=5 // 2,
+                      groups=self.dim, padding_mode='reflect'),
+            nn.GELU()
+        )
+
+    def forward(self, x):
+        x = self.conv_init(x)
+        x = list(torch.split(x, self.dim, dim=1))
+        x_local_1 = self.dw_conv_1(x[0])
+        x_local_2 = self.dw_conv_2(x[0])
+        x_gloal = self.mixer_gloal(torch.cat([x_local_1, x_local_2], dim=1))
+        x = self.ca_conv(x_gloal)
+        x = self.ca(x) * x
+
+        return x
+
+
+class C3k_FFCM(C3k):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5, k=3):
+        super().__init__(c1, c2, n, shortcut, g, e, k)
+        c_ = int(c2 * e)  # hidden channels
+        self.m = nn.Sequential(*(Fused_Fourier_Conv_Mixer(c_) for _ in range(n)))
+
+
+class C3k2_FFCM(C3k2):
+    def __init__(self, c1, c2, n=1, c3k=False, e=0.5, g=1, shortcut=True):
+        super().__init__(c1, c2, n, c3k, e, g, shortcut)
+        self.m = nn.ModuleList(
+            C3k_FFCM(self.c, self.c, 2, shortcut, g) if c3k else Fused_Fourier_Conv_Mixer(self.c) for _ in range(n))
+
+######################################## FFCM end ########################################
+######################################## SPD-Conv start ########################################
+
+class SPDConv(nn.Module):
+    # Changing the dimension of the Tensor
+    def __init__(self, inc, ouc, dimension=1):
+        super().__init__()
+        self.d = dimension
+        self.conv = Conv(inc * 4, ouc, k=3)
+
+    def forward(self, x):
+        x = torch.cat([x[..., ::2, ::2], x[..., 1::2, ::2], x[..., ::2, 1::2], x[..., 1::2, 1::2]], 1)
+        x = self.conv(x)
+        return x
+
+######################################## SPD-Conv end ########################################
+######################################## Omni-Kernel Network for Image Restoration [AAAI-24] start ########################################
+
+class FGM(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        self.conv = nn.Conv2d(dim, dim*2, 3, 1, 1, groups=dim)
+
+        self.dwconv1 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.dwconv2 = nn.Conv2d(dim, dim, 1, 1, groups=1)
+        self.alpha = nn.Parameter(torch.zeros(dim, 1, 1))
+        self.beta = nn.Parameter(torch.ones(dim, 1, 1))
+
+    def forward(self, x):
+        # res = x.clone()
+        fft_size = x.size()[2:]
+        x1 = self.dwconv1(x)
+        x2 = self.dwconv2(x)
+
+        x2_fft = torch.fft.fft2(x2, norm='backward')
+
+        out = x1 * x2_fft
+
+        out = torch.fft.ifft2(out, dim=(-2,-1), norm='backward')
+        out = torch.abs(out)
+
+        return out * self.alpha + x * self.beta
+
+class OmniKernel(nn.Module):
+    def __init__(self, dim) -> None:
+        super().__init__()
+
+        ker = 31
+        pad = ker // 2
+        self.in_conv = nn.Sequential(
+                    nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1),
+                    nn.GELU()
+                    )
+        self.out_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1)
+        self.dw_13 = nn.Conv2d(dim, dim, kernel_size=(1,ker), padding=(0,pad), stride=1, groups=dim)
+        self.dw_31 = nn.Conv2d(dim, dim, kernel_size=(ker,1), padding=(pad,0), stride=1, groups=dim)
+        self.dw_33 = nn.Conv2d(dim, dim, kernel_size=ker, padding=pad, stride=1, groups=dim)
+        self.dw_11 = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=dim)
+
+        self.act = nn.ReLU()
+
+        ### sca ###
+        self.conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.pool = nn.AdaptiveAvgPool2d((1,1))
+
+        ### fca ###
+        self.fac_conv = nn.Conv2d(dim, dim, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        self.fac_pool = nn.AdaptiveAvgPool2d((1,1))
+        self.fgm = FGM(dim)
+
+    def forward(self, x):
+        out = self.in_conv(x)
+
+        ### fca ###
+        x_att = self.fac_conv(self.fac_pool(out))
+        x_fft = torch.fft.fft2(out, norm='backward')
+        x_fft = x_att * x_fft
+        x_fca = torch.fft.ifft2(x_fft, dim=(-2,-1), norm='backward')
+        x_fca = torch.abs(x_fca)
+
+        ### fca ###
+        ### sca ###
+        x_att = self.conv(self.pool(x_fca))
+        x_sca = x_att * x_fca
+        ### sca ###
+        x_sca = self.fgm(x_sca)
+
+        out = x + self.dw_13(out) + self.dw_31(out) + self.dw_33(out) + self.dw_11(out) + x_sca
+        out = self.act(out)
+        return self.out_conv(out)
+
+class CSPOmniKernel(nn.Module):
+    def __init__(self, dim, e=0.25):
+        super().__init__()
+        self.e = e
+        self.cv1 = Conv(dim, dim, 1)
+        self.cv2 = Conv(dim, dim, 1)
+        self.m = OmniKernel(int(dim * self.e))
+
+    def forward(self, x):
+        ok_branch, identity = torch.split(self.cv1(x), [int(self.cv1.conv.out_channels * self.e), int(self.cv1.conv.out_channels * (1 - self.e))], dim=1)
+        return self.cv2(torch.cat((self.m(ok_branch), identity), 1))
+
+######################################## Omni-Kernel Network for Image Restoration [AAAI-24] end ########################################
